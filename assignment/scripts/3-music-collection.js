f381a050fcf4049d88022da6de9b7d8d6cefeab2
Tim Miner <poeminminer1@gmail.com>
2022-06-04 21:12:53 -0500

    working addToCollection function... Commented out constructor. Not sure if its okay to use yet

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+//3 parameter construtor
+//  function album (title, artist, yearPublished) {
+//      this.title = title;
+//      this.artist = artist;
+//      this.yearPublished = yearPublished;
+//  }
+
+ // empty array
+let collection = [];
+
+//using this. did not work when pointing at the parameters?
+function addToCollection(addTitle, addArtist, addYearPublished) {
+    let album = {
+        title: addTitle,
+        artist: addArtist,
+        yearPublished: addYearPublished
+    };
+    collection.push(album);
+}
+addToCollection("test", "MINER", 1991);
+console.log(collection);
+addToCollection("test2", "MEEEEEE", 1000);
+console.log(collection);
